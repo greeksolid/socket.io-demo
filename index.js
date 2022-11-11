657d903aa2496336c2c5d3c82be3ab294ac69ae9
Tasos Georgoulis <tasosgeorgoulis@gmail.com>
2022-11-11 02:22:15 +0200

    .

--- index.js
+++ index.js
@@ -1,43 +1,47 @@
-const app = require('express')()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:8080",
+  },
+});
 
-var screen
+var screen;
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/screen.html')
-})
-app.get('/r', (req, res) => {
-    res.sendFile(__dirname + '/remote.html')
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/screen.html");
+});
+app.get("/r", (req, res) => {
+  res.sendFile(__dirname + "/remote.html");
+});
 
-io.on('connection', (socket) => {
-    console.log(`${socket.id} connected!`)
-    socket.emit('hello', {hello: 'world'})
-    socket.on('hello yourself', (data) => {
-        console.log(data)
-    })
+io.on("connection", (socket) => {
+  console.log(`${socket.id} connected!`);
+  socket.emit("hello", { hello: "world" });
+  socket.on("hello yourself", (data) => {
+    console.log(data);
+  });
 
-    socket.on('registerScreen', (data) => {
-        screen = {}
-        screen.id = socket.id
-    })
+  socket.on("registerScreen", (data) => {
+    screen = {};
+    screen.id = socket.id;
+  });
 
-    socket.on('sendToScreen', (data) => {
-        io.to(screen.id).emit('fromRemote', data)// relay from remote to screen!
-    })
+  socket.on("sendToScreen", (data) => {
+    io.to(screen.id).emit("fromRemote", data); // relay from remote to screen!
+  });
 
-    socket.on('changeColor', () => {
-        io.to(screen.id).emit('remoteWantsNewColor', socket.id)
-    })
+  socket.on("changeColor", () => {
+    io.to(screen.id).emit("remoteWantsNewColor", socket.id);
+  });
 
-    socket.on('disconnect', (data) => {
-        console.log(`${socket.id} disconnected. :(`)
-    })
+  socket.on("disconnect", (data) => {
+    console.log(`${socket.id} disconnected. :(`);
+  });
 
-    socket.on('thanks', (data) => {
-        io.to(data).emit('thanks', socket.id)
-    })
-})
+  socket.on("thanks", (data) => {
+    io.to(data).emit("thanks", socket.id);
+  });
+});
 
-server.listen(3000)
+server.listen(3000);
